0daabc48deda2c7c7eb684d19b8a0ccb361173d8
GaabDiaSs <gabriel.c.dias22@gmail.com>
2023-09-11 09:02:55 -0300

    first commit

---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,12 @@
+<h3>Principal (VIEW) </h3>
+
+
+<ul>
+    <li>
+        <a href="/sobrenos"> Sobre Nós </a>
+    </li>
+
+    <li>
+        <a href="/contato"> Contato </a>
+    </li>
+</ul>
---
+++ resources/views/site/sobrenos.blade.php
@@ -0,0 +1,16 @@
+<h3>Sobre nós (VIEW) </h3>
+
+<ul>
+    <li>
+        <a href="/"> Principal </a>
+    </li>
+
+    <li>
+        <a href="/sobrenos"> Sobre Nós </a>
+    </li>
+
+    <li>
+        <a href="/contato"> Contato </a>
+    </li>
+
+</ul>
